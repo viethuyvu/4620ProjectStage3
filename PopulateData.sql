85bc19ac0dcb95bd3fbbb4d15ae33d6891863eff
Viet Huy Vu <viethuyvu@gmail.com>
2024-11-19 21:19:01 -0500

    add populate data, orders left

---
+++ PopulateData.sql
@@ -0,0 +1,43 @@
+INSERT INTO Topping VALUES ('1','Pepperoni','1.25','0.2','100','2','2.75','3.5','4.5');
+INSERT INTO Topping VALUES ('2','Sausage','1.25','0.15','100','2.5','3','3.5','4.25');
+INSERT INTO Topping VALUES ('3','Ham','1.5','0.15','78','2','2.5','3.25','4');
+INSERT INTO Topping VALUES ('4','Chicken','1.75','0.25','56','1.5','2','2.25','3');
+INSERT INTO Topping VALUES ('5','Green Pepper','0.5','0.02','79','1','1.5','2','2.5');
+INSERT INTO Topping VALUES ('6','Onion','0.5','0.02','85','1','1.5','2','2.75');
+INSERT INTO Topping VALUES ('7','Roma tomato','0.75','0.03','86','2','3','3.5','4.5');
+INSERT INTO Topping VALUES ('8','Mushrooms','0.75','0.1','52','1.5','2','2.5','3');
+INSERT INTO Topping VALUES ('9','Black Olives','0.6','0.1','39','0.75','1','1.5','2');
+INSERT INTO Topping VALUES ('10','Pineapple','1','0.25','15','1','1.25','1.75','2');
+INSERT INTO Topping VALUES ('11','Jalapenos','0.5','0.05','64','0.5','0.75','1.25','1.75');
+INSERT INTO Topping VALUES ('12','Banana Peppers','0.5','0.05','36','0.6','1','1.3','1.75');
+INSERT INTO Topping VALUES ('13','Regular Cheese','1.5','0.12','250','2','3.5','5','7');
+INSERT INTO Topping VALUES ('14','Four Cheese Blend','2','0.15','150','2','3.5','5','7');
+INSERT INTO Topping VALUES ('15','Feta Cheese','2','0.18','75','1.75','3','4','5.5');
+INSERT INTO Topping VALUES ('16','Goat Cheese','2','0.2','54','1.6','2.75','4','5.5');
+INSERT INTO Topping VALUES ('17','Bacon','1.5','0.25','89','1','1.5','2','3');
+
+INSERT INTO Discount VALUES('1','Employee','15', NULL);
+INSERT INTO Discount VALUES('2','Lunch Special Medium',NULL, '1');
+INSERT INTO Discount VALUES('3','Lunch Special Large',NULL, '2');
+INSERT INTO Discount VALUES('4','Specialty Pizza',NULL, '1.5');
+INSERT INTO Discount VALUES('5','Gameday special','20', NULL);
+
+INSERT INTO Base_Price VALUES ('1','Small','Thin','3','0.5');
+INSERT INTO Base_Price VALUES ('2','Small','Original','3','0.75');
+INSERT INTO Base_Price VALUES ('3','Small','Pan','3.5','1');
+INSERT INTO Base_Price VALUES ('4','Small','Gluten-Free','4','2');
+INSERT INTO Base_Price VALUES ('5','Medium','Thin','5','1');
+INSERT INTO Base_Price VALUES ('6','Medium','Original','5','1.5');
+INSERT INTO Base_Price VALUES ('7','Medium','Pan','6','2.25');
+INSERT INTO Base_Price VALUES ('8','Medium','Gluten-Free','6.25','3');
+INSERT INTO Base_Price VALUES ('9','Large','Thin','8','1.25');
+INSERT INTO Base_Price VALUES ('10','Large','Original','8','2');
+INSERT INTO Base_Price VALUES ('11','Large','Pan','9','3');
+INSERT INTO Base_Price VALUES ('12','Large','Gluten-Free','9.5','4');
+INSERT INTO Base_Price VALUES ('13','X-Large','Thin','10','2');
+INSERT INTO Base_Price VALUES ('14','X-Large','Original','10','3');
+INSERT INTO Base_Price VALUES ('15','X-Large','Pan','11.5','4.5');
+INSERT INTO Base_Price VALUES ('16','X-Large','Gluten-Free','12.5','6');
+
+
+
